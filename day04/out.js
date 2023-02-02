d0760d9e9413184a2c2ff47a31d553828043af0b
Tysley <124110978+Tysley@users.noreply.github.com>
2023-02-02 17:42:24 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,14 @@
+function 빨래하다() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다")
+    console.log("5. 물에 여러번 헹군다")
+    console.log("6. 물을 짠다")
+    console.log("7. 햇빛에 넌다.")
+}
+function 청소하다(){
+    console.log("1. 청소기를 돌린다")
+    console.log("2. 먼지를 턴다")
+    console.log("3. 바닥을 닦는다")
+}
---
+++ day04/out2.js
@@ -0,0 +1,14 @@
+function 여행가다(){
+    console.log("1.여권과 짐을 준비한다.")
+    console.log("2.숙소를 예약한다")
+    console.log("3.비행기를 타고 여행지로 간다")
+    console.log("4.즐긴다!")
+}
+
+function 쉬다(){
+    console.log("1.집으로 간다")
+    console.log("2.몸을 씻는다")
+    console.log("3.머리를 말린다")
+    console.log("4.침대로 들어간다")
+    console.log("5.잔다")
+}
